e9525ffc0b1972d039d89dc1ab3f7dc857bca6ee
Mariko Mizuhara <marimiz@Mariko-Mizuharas-MacBook-Pro.local>
2019-01-16 19:02:43 -0800

    SingleMovieServlet working

---
+++ project1/src/SingleMovieServlet.java
@@ -0,0 +1,157 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SingleMovieServlet
+ */
+@WebServlet("/SingleMovieServlet")
+public class SingleMovieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SingleMovieServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		
+		//response.getWriter().append("Served at: ").append(request.getParameter("moviename"));
+		
+		String movie_to_search = request.getParameter("moviename");
+		
+		 // change this to your own mysql username and password
+        String loginUser = "mytestuser";
+        String loginPasswd = "mypassword";
+        String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+		
+        // set response mime type
+        response.setContentType("text/html"); 
+
+        // get the printwriter for writing response
+        PrintWriter out = response.getWriter();
+        //set up html page
+        out.println("<html>");
+        out.println("<head><title>Fabflix</title></head>");
+        
+        
+        try {
+        		Class.forName("com.mysql.jdbc.Driver").newInstance();
+        		// create database connection
+        		Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+        		// declare statement
+        		Statement statement = connection.createStatement();
+        		// prepare query, custom made for this problem
+        		String query =  "SELECT * FROM movies as m\r\n" + 
+        				"JOIN  ratings as r ON r.movieId = m.id\r\n" + 
+        				"\r\n" + 
+        				"join (\r\n" + 
+        				"select title, group_concat(name) as genres from genres_in_movies join genres on genres_in_movies.genreId = genres.id \r\n" + 
+        				"join movies on genres_in_movies.movieId = movies.id Group by title\r\n" + 
+        				") as gm\r\n" + 
+        				"ON gm.title = m.title\r\n" + 
+        				"\r\n" + 
+        				"join (\r\n" + 
+        				"select title, group_concat(name) as stars from stars_in_movies join stars on stars_in_movies.starId = stars.id \r\n" + 
+        				"join movies on stars_in_movies.movieId = movies.id Group by title\r\n" + 
+        				") as sm\r\n" + 
+        				"ON sm.title = m.title\r\n" + 
+        				"\r\n" + 
+        				"ORDER BY r.rating desc\r\n" + 
+        				"limit 20\r\n" + 
+        				"" ;
+        		// execute query, taken from example
+        		ResultSet resultSet = statement.executeQuery(query);
+        		//set up body
+        		out.println("<body>");
+        		out.println("<center>"); // hopefully will make it look nicer 
+
+        		out.println("<table border>");
+        		
+        		// set up table header
+        		out.println("<tr>");
+        		out.println("<td>year</td>");
+        		out.println("<td>director</td>");
+        		out.println("<td>genres</td>");
+        		out.println("<td>stars</td>");
+        		out.println("<td>rating</td>");
+        		out.println("</tr>");
+        		
+        		while (resultSet.next()) {
+        			String title = resultSet.getString("title");
+        			String year = resultSet.getString("year");
+        			String director = resultSet.getString("director");
+        			String genres = resultSet.getString("genres");
+        			String stars = resultSet.getString("stars");
+        			String rating = resultSet.getString("rating");
+        			
+        			if (title.equals(movie_to_search))
+        			{
+        				out.println("<h1>" + title + "</h1>");
+	        			out.println("<tr>");
+	        			
+	        			out.println("<td>" + year + "</td>");
+	        			out.println("<td>" + director + "</td>");
+	        			out.println("<td>" + genres + "</td>");
+	        			out.println("<td>" + stars + "</td>");
+	        			out.println("<td>" + rating + "</td>");
+	        			out.println("</tr>");
+        			}
+        		}
+        		
+        		out.println("</table>");
+        		out.println("</center>");
+        		out.println("</body>");
+        		resultSet.close();
+        		statement.close();
+        		connection.close();
+        		
+		
+        } catch (Exception e) {
+        		/*
+        		 * After you deploy the WAR file through tomcat manager webpage,
+        		 *   there's no console to see the print messages.
+        		 * Tomcat append all the print messages to the file: tomcat_directory/logs/catalina.out
+        		 * 
+        		 * To view the last n lines (for example, 100 lines) of messages you can use:
+        		 *   tail -100 catalina.out
+        		 * This can help you debug your program after deploying it on AWS.
+        		 */
+        		e.printStackTrace();
+        		
+        		out.println("<body>");
+        		out.println("<p>");
+        		out.println("Exception in doGet: " + e.getMessage());
+        		out.println("</p>");
+        		out.print("</body>");
+        }
+        
+        out.println("</html>");
+        out.close();
+        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
